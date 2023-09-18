83474439b59f946f03a095a3df16dafd21bfd625
Azhar <darkcoders0101@gmail.com>
2023-09-18 21:51:44 +0530

    Create Day18.cpp

---
+++ September_daily_question_sol/Day18.cpp
@@ -0,0 +1,23 @@
+//?  1337. The K Weakest Rows in a Matrix
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+      priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+      for(int i=0; i<mat.size(); i++) {
+        int cur = 0;
+        for(int j=0; j<mat[i].size(); j++) {
+          if(mat[i][j]==1)
+            cur++;
+        }
+        pq.push({cur, i});
+      } 
+      vector<int> ans;
+      while(k--) {
+        int top = pq.top().second;
+        pq.pop();
+        ans.push_back(top);
+      }
+      return ans;
+    }
+};
