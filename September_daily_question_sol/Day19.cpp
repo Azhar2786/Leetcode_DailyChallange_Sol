6de7d1b39f6d6e04b16f85752eb8df2e6c29fd22
Azhar <darkcoders0101@gmail.com>
2023-09-19 19:00:20 +0530

    Create Day19.cpp

---
+++ September_daily_question_sol/Day19.cpp
@@ -0,0 +1,20 @@
+//!    287. Find the Duplicate Number
+
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int ans = -1;
+        for(int i=0; i<nums.size(); i++){
+            int index = abs(nums[i]);
+
+            if(nums[index]<0){
+                ans = index;
+                break;
+            }
+           nums[index] *= -1;
+        }
+        return ans;
+    }
+};
