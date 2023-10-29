06dd441fddc3c7b506214f40574d424a33c7dc4f
Azhar <darkcoders0101@gmail.com>
2023-10-29 18:04:04 +0530

    458. Poor Pigs

---
+++ October_daily_question/Day29.cpp
@@ -0,0 +1,16 @@
+//!  458. Poor Pigs
+
+
+class Solution {
+public:
+
+#define MAX_PIGS 10
+int poorPigs(int buckets, int poisonTime, int totalTime) {
+    int base = totalTime/poisonTime + 1;
+    for (int i = 0; i < MAX_PIGS; i++) {
+        if (powl(base,i) >= buckets)
+            return i;
+    }
+    return MAX_PIGS;
+}
+};
