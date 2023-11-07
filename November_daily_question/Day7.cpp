eafb8ceca577a070ac1440009da1c645551bb99c
Azhar <darkcoders0101@gmail.com>
2023-11-07 18:42:09 +0530

    1921. Eliminate Maximum Number of Monsters

---
+++ November_daily_question/Day7.cpp
@@ -0,0 +1,21 @@
+//!  1921. Eliminate Maximum Number of Monsters
+
+
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int n = dist.size();
+        vector<int>time;
+        for(int i=0; i<n; i++){
+            time.push_back((dist[i] + speed[i] -1)/speed[i]);
+        }
+        sort(time.begin(),time.end());
+
+        for(int i=0; i<n;i++){
+            if(i >= time[i]){
+                return i;
+            }
+        }
+        return n;
+    }
+};
