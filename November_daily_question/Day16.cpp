2c091f008eb87a4174c1881fa3987a0a9cde8a2b
Azhar <darkcoders0101@gmail.com>
2023-11-16 17:04:52 +0530

    1980. Find Unique Binary String

---
+++ November_daily_question/Day16.cpp
@@ -0,0 +1,15 @@
+//!  1980. Find Unique Binary Strin
+
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string ans;
+        for(int i=0; i<nums.size(); i++){
+            char curr = nums[i][i];
+            ans += curr == '0' ? '1' : '0';
+        }
+
+        return ans;
+    }
+};
