3ec982dcc8ae4f01cc9d8deeab46b9882184ae50
Azhar <darkcoders0101@gmail.com>
2023-12-03 14:37:06 +0530

    1266. Minimum Time Visiting All Points

---
+++ December-daiy_question/Day3.cpp
@@ -0,0 +1,17 @@
+//!  1266. Minimum Time Visiting All Points
+
+
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0, n = points.size();
+
+        for(int i=1; i<n; i++){
+            ans += max(abs(points[i][0] - points[i-1][0]),
+                       abs(points[i][1] - points[i-1][1]));
+        }
+
+        return ans;
+    }
+};
