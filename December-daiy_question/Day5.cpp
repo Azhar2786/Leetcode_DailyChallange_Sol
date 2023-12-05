2b564cdb2c47ed8458c9b4a399a5c5b543cdefa4
Azhar <darkcoders0101@gmail.com>
2023-12-05 13:04:00 +0530

    1688. Count of Matches in Tournament

---
+++ December-daiy_question/Day5.cpp
@@ -0,0 +1,18 @@
+//!  1688. Count of Matches in Tournament
+
+
+
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int ans = 0 ;
+        while(n > 0){
+          ans = ans + n/2;
+          n = (n+1)/2;
+          if(n==1)
+          break;
+        }
+        return ans;
+    }
+};
