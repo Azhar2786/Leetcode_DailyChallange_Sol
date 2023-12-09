fd170c0fec7be3a962a7f89a728a44ff127d3d0f
Azhar <darkcoders0101@gmail.com>
2023-12-09 17:10:39 +0530

    94. Binary Tree Inorder Traversal

---
+++ December-daiy_question/Day9.cpp
@@ -0,0 +1,48 @@
+//! 94. Binary Tree Inorder Traversal
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        TreeNode* curr = root;
+        while(curr){
+            if(curr->left == NULL){
+                ans.push_back(curr->val);
+                curr = curr->right;
+            }
+            else{
+
+                TreeNode* pre = curr->left;
+                while(pre->right != curr && pre->right){
+                    pre = pre->right;
+                }
+
+                if(pre->right == NULL){
+                    pre->right = curr;
+                    curr = curr->left;
+                }
+                else{
+                    pre->right = NULL;
+                    ans.push_back(curr->val);
+                    curr = curr->right;
+                }
+            }
+        }
+        return ans;
+    }
+};
