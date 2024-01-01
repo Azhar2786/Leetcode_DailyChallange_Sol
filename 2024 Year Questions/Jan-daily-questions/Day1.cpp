768c44e7b6c59cbaf88c50d858c23ebf6b2df943
Azhar <darkcoders0101@gmail.com>
2024-01-01 11:48:51 +0530

    455. Assign Cookies

---
+++ 2024 Year Questions/Jan-daily-questions/Day1.cpp
@@ -0,0 +1,21 @@
+//! 455. Assign Cookies
+
+
+
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int ans = 0, i = 0;
+        for(int j=0; j<s.size(); j++){
+            if(i<g.size() && g[i] <= s[j]){
+                ans++;
+                i++;
+            }
+        }
+        return ans;
+    }
+};
